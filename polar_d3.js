569e31d0218482ab6de36a2860c1df1321c669bb
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-11-02 21:07:41 +0000

    removed external code

--- polar_d3.js
+++ polar_d3.js
@@ -2,20 +2,6 @@ var screen_width=screen.availWidth;
 var width = Math.floor(parseInt((screen_width*0.35)));
 var height = Math.floor(parseInt((screen_width*0.35)));
 var radius = Math.min(width, height);
-var spacing = .09;
-var formatSecond = d3.time.format("%S s");
-var formatMinute = d3.time.format("%M m");
-var formatHour = d3.time.format("%H h");
-
-var color = d3.scale.linear()
-.range(["hsl(-180,50%,50%)", "hsl(180,50%,50%)"])
-.interpolate(interpolateHsl);
-
-var arc = d3.svg.arc()
-.startAngle(0)
-.endAngle(function(d){return d.value * 2 * Math.PI;})
-.innerRadius(function(d){return d.index * radius;})
-.outerRadius(function(d){return (d.index + spacing) * radius;});
 
 var svg = d3.select("#chart").append("svg")
 .attr("width", width)
@@ -29,28 +15,6 @@ var flip_rotation = document.getElementById("chart");
 flip_rotation.addEventListener("click", function(e){var target = e.target || e.srcElement; switch_rotation()}, false);
 //console.log("in switch_rotation: " + rotation_direction);
 };
-
-var field = svg.selectAll("g")
-.data(fields)
-.enter().append("g");
-
-field.append("path");
-d3.transition().duration(0).each(tick);
-d3.select(self.frameElement).style("height", height + "px");
-
-function tick(){
-field = field
-.each(function(d) {this._value = d.value;})
-.data(fields)
-.each(function(d) {d.previousValue = this._value;});
-
-field.select("path")
-.transition()
-.ease("elastic")
-.attrTween("d", arcTween)
-.style("fill", function(d) {return color(d.value);});
-
-setTimeout(tick, 1000 - Date.now() % 1000);}
 
 function number(a, b){
 if (rotation_direction == 0){a = -a, b = -b;} // change direction to counter clockwise
@@ -83,7 +47,7 @@ value: now.getMinutes() / 60},
 {index: .1,
 value: now.getHours() / 24}];}
 
-function interpolateHsl(a, b) {
+function interpolate(a, b) {
 var i = d3.interpolateString(a, b);
 return function(t) {
 return d3.hsl(i(t));};}
